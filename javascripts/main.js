618f8c50667db51962d278226c952c99a01ba859
Jens Bannmann <jensbannmann@incub8.de>
2015-08-28 11:00:32 +0200

    Added word list and notes; display password and word list size on page.
    Conflicts:
    	index.html

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,12 +1,14 @@
 var request = new XMLHttpRequest();
 request.addEventListener("load", function() {
     words = JSON.parse(this.responseText);
+    document.getElementById("wordCount").innerHTML = words.length;
     generate(4);
 });
 request.open("get", "words.json", true);
 request.send();
 
 function generate(passwordLength) {
+    var chosenWords = [];
     if ("crypto" in window && "getRandomValues" in window.crypto) {
         var maxUint32 = Math.pow(2,32) - 1;
         var array = new Uint32Array(passwordLength);
@@ -15,10 +17,11 @@ function generate(passwordLength) {
         for (var i = 0; i < array.length; i++) {
             // Rounding down means we never reach words.length, which is good because the highest array indexes is length-1.
             var index = Math.floor(array[i] / maxUint32 * words.length);
-            console.log(words[index]);
+            chosenWords.push(words[index]);
         }
     } else {
         // TODO: fall back to Math.random()
         throw "Unsupported browser";
     }
+    document.getElementById("generatedPassword").innerHTML = chosenWords.join(" ");
 }
