b2b3c686f7fa0314af1eeebd3a0896221a04b0f2
Jens Bannmann <jens.b@web.de>
2015-08-28 08:08:27 +0200

    Implemented word selection.
    - Only crypto.getRandomValues() for now.

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,1 +1,24 @@
-console.log('This would be the main JS file.');
+var request = new XMLHttpRequest();
+request.addEventListener("load", function() {
+    words = JSON.parse(this.responseText);
+    generate(4);
+});
+request.open("get", "words.json", true);
+request.send();
+
+function generate(passwordLength) {
+    if ("crypto" in window && "getRandomValues" in window.crypto) {
+        var maxUint32 = Math.pow(2,32) - 1;
+        var array = new Uint32Array(passwordLength);
+        window.crypto.getRandomValues(array);
+
+        for (var i = 0; i < array.length; i++) {
+            // Rounding down means we never reach words.length, which is good because the highest array indexes is length-1.
+            var index = Math.floor(array[i] / maxUint32 * words.length);
+            console.log(words[index]);
+        }
+    } else {
+        // TODO: fall back to Math.random()
+        throw "Unsupported browser";
+    }
+}
