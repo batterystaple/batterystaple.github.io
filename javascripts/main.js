f9f2ff6b6893bc10444fa335baa8aded2a12c892
Jens Bannmann <jens.b@web.de>
2015-09-03 21:07:17 +0200

    Number of words is now configurable.
    - resolves #1

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,22 +1,35 @@
+var passwordLengthSelect = document.getElementById("passwordLengthSelect");
+
+var passwordLength = 4;
+if ("passwordLength" in localStorage) {
+    passwordLength = parseInt(localStorage.passwordLength);
+    passwordLengthSelect.value = passwordLength;
+}
+
+passwordLengthSelect.addEventListener("change", function(event) {
+    passwordLength = parseInt(this.value);
+    localStorage.passwordLength = passwordLength;
+});
+
 var request = new XMLHttpRequest();
 request.addEventListener("load", function() {
     words = JSON.parse(this.responseText);
     document.getElementById("wordCount").innerHTML = words.length;
-    generate(4);
+    generate();
 });
 request.open("get", "words.json", true);
 request.send();
 
-function generate(passwordLength) {
+function generate() {
     var chosenWords = [];
     if ("crypto" in window && "getRandomValues" in window.crypto) {
-        var maxUint32 = Math.pow(2,32) - 1;
-        var array = new Uint32Array(passwordLength);
-        window.crypto.getRandomValues(array);
+        var numbers = new Uint32Array(passwordLength);
+        window.crypto.getRandomValues(numbers);
 
-        for (var i = 0; i < array.length; i++) {
-            // Rounding down means we never reach words.length, which is good because the highest array indexes is length-1.
-            var index = Math.floor(array[i] / maxUint32 * words.length);
+        var maxUint32 = Math.pow(2,32) - 1;
+        for (var i = 0; i < numbers.length; i++) {
+            // Rounding down means we never reach words.length, which is good because the highest array index is length-1.
+            var index = Math.floor(numbers[i] / maxUint32 * words.length);
             chosenWords.push(words[index]);
         }
     } else {
