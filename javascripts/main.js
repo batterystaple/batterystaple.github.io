8431e38cc7fcbcc43cd44726be15a67aabd782b0
Jens Bannmann <jens.b@web.de>
2015-09-30 23:16:53 +0200

    Adding spaces is now optional.
    - Improved layout.
    - resolves #6

--- javascripts/main.js
+++ javascripts/main.js
@@ -11,10 +11,22 @@ passwordLengthSelect.addEventListener("change", function(event) {
     localStorage.passwordLength = passwordLength;
 });
 
+var useSpacesCheckbox = document.getElementById("useSpacesCheckbox");
+var useSpaces = false;
+if ("useSpaces" in localStorage) {
+    useSpaces = (localStorage.useSpaces == "true");
+    useSpacesCheckbox.checked = useSpaces;
+}
+
+useSpacesCheckbox.addEventListener("change", function(event) {
+    useSpaces = useSpacesCheckbox.checked;
+    localStorage.useSpaces = useSpaces;
+});
+
 var request = new XMLHttpRequest();
 request.addEventListener("load", function() {
     words = this.responseText.split(/\n/);
-    display("wordCount", words.length);
+    document.getElementById("wordCount").textContent = words.length;
     generate();
 });
 request.open("get", "words.txt", true);
@@ -25,21 +37,24 @@ function generate() {
         var numbers = new Uint32Array(passwordLength);
         window.crypto.getRandomValues(numbers);
 
-        var chosenWords = [];
+        var generatedPassword = document.getElementById("generatedPassword");
         var maxUint32 = Math.pow(2,32) - 1;
         for (var i = 0; i < numbers.length; i++) {
             // Rounding down means we never reach words.length, which is good because the highest array index is length-1.
             var index = Math.floor(numbers[i] / maxUint32 * words.length);
-            chosenWords.push(words[index]);
+
+            var text = words[index];
+            if (useSpaces && i < numbers.length - 1) {
+                text += " ";
+            }
+
+            var span = document.createElement("span");
+            span.appendChild(document.createTextNode(text));
+            generatedPassword.appendChild(span);
         }
-        display("generatedPassword", chosenWords.join(" "));
     } else {
         showError("Your browser does not support the required feature 'window.crypto'. Try upgrading.");
     }
-}
-
-function display(id, content) {
-    document.getElementById(id).textContent = content;
 }
 
 function showError(message) {
